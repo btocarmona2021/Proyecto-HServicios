a8225aca4ad73a136221f23f884ad8c4ff9563df
Andres Zampar <andreszampar@gmail.com>
2023-11-10 19:22:59 -0300

    controlador_prueba

--- src/main/java/com/equipoh/hservicios/controladores/PortalControlador.java
+++ src/main/java/com/equipoh/hservicios/controladores/PortalControlador.java
@@ -3,7 +3,12 @@
 package com.equipoh.hservicios.controladores;
 
 
+import com.equipoh.hservicios.entidades.Usuario;
+import com.equipoh.hservicios.servicios.UsuarioService;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -12,10 +17,14 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class PortalControlador {
 
    
-
+ @Autowired
+    private UsuarioService usuarioService;
    
     @GetMapping("/")
-    public String index() {
+    public String index(ModelMap modelo) {
+         List<Usuario> usuarios = usuarioService.listarUsuarios();
+        modelo.addAttribute("usuarios", usuarios);
+
      return "index.html";
     }
 
@@ -31,5 +40,29 @@ public class PortalControlador {
     public String panel() {
         return "panel.html";
     }
-    
+    @GetMapping("/list")
+    public String listar(ModelMap modelo) {
+        List<Usuario> usuarios = usuarioService.listarUsuarios();
+        modelo.addAttribute("usuarios", usuarios);
+
+        return "listar_usuario.html";
+    }
+    @GetMapping("/active")
+    public String active() {
+        return "active.html";
+    }
+    @GetMapping("/modificar")
+    public String modificar() {
+        return "modificar_usuario.html";
+    }
+     @GetMapping("/buscador")
+    public String buscador(ModelMap modelo) {
+        List<Usuario> usuarios = usuarioService.listarUsuarios();
+        modelo.addAttribute("usuarios", usuarios);
+        return "buscador.html";
+    }
+     @GetMapping("/contrato")
+    public String contrato() {
+        return "contratarService.html";
+    }
 }
