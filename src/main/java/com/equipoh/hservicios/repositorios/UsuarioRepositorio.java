7d1f97aa953e448970b0953e31e8694cf128532d
Alberto C <btocarmona@gmail.com>
2023-11-10 00:16:56 -0300

    Agregue entidad Usuario,Rol,UsuarioRepositorio

--- src/main/java/com/equipoh/hservicios/repositorios/UsuarioRepositorio.java
+++ src/main/java/com/equipoh/hservicios/repositorios/UsuarioRepositorio.java
@@ -7,10 +7,11 @@ import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface UsuarioRepositorio extends JpaRepository<Usuario,String>{
-    
+public interface UsuarioRepositorio extends JpaRepository<Usuario, String> {
     // query de prueba
+    // COMO NO PEDIMOS NOMBRE DE USUARIO SEGURAMENTE VAMOS A USAR LOGUEO POR MAIL
+    //HABRIA QUE ADAPTAR LA QUERY
     @Query("SELECT u FROM Usuario u WHERE u.nombre = :nombre")
-    public Usuario buscarPorNombre (@Param ("nombre") String nombre);
-    
+    public Usuario buscarPorNombre(@Param("nombre") String nombre);
+
 }
