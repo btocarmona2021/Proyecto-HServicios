096c77d38d698537969e9b5a8a3aab18067eb6e5
TinchoBus <bustincho@gmail.com>
2023-11-11 20:41:29 -0300

    ENVIO TRABAJO DE IMAGEN

---
+++ src/main/java/com/equipoh/hservicios/controladores/ImagenControlador.java
@@ -0,0 +1,46 @@
+
+package com.equipoh.hservicios.controladores;
+
+import com.equipoh.hservicios.entidades.Usuario;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+/**
+ *
+ * @author Martb
+ */
+@Controller
+@RequestMapping("/imagen")
+public class ImagenControlador {
+    
+    @Autowired
+   // UsuarioServicio usuarioServicio; FALTA USUARIO SERVICIO
+    
+    @GetMapping("/perfil/{id}")
+    public ResponseEntity<byte[]>imagenUsuario(@PathVariable String id){
+        
+        Usuario usuario = usuarioServicio.getOne(id); //FALTA USUARIO SERVICIO
+        
+       byte[] imagen = usuario.getImagen().getContenido();
+       
+       HttpHeaders headers = new HttpHeaders();
+       
+       headers.setContentType(MediaType.IMAGE_JPEG);
+       
+       
+       
+       return new ResponseEntity<>(imagen, headers,HttpStatus.OK);
+    
+    }
+    
+    
+    
+}
---
+++ src/main/java/com/equipoh/hservicios/entidades/Imagen.java
@@ -0,0 +1,44 @@
+
+package com.equipoh.hservicios.entidades;
+
+import javax.persistence.Basic;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Lob;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+import org.hibernate.annotations.GenericGenerator;
+
+/**
+ *
+ * @author Martb
+ */
+
+@Entity
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+public class Imagen {
+    @Id
+    @GeneratedValue(generator = "uuid")
+    @GenericGenerator(name = "uuid", strategy = "uuid2")
+    private String id;
+    
+    private String mime;
+    
+    private String nombre;
+    
+    @Lob @Basic(fetch =FetchType.LAZY)
+    private byte[] contenido;
+    
+    
+    
+    
+    
+    
+}
