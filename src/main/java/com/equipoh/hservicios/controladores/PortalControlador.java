36b2cbacd9281ec4a702347dd969dd7e16b72461
aveja85 <avgestora@gmail.com>
2023-11-14 21:01:43 -0300

    Resolucion de ERRORES en rama ANA

--- src/main/java/com/equipoh/hservicios/controladores/PortalControlador.java
+++ src/main/java/com/equipoh/hservicios/controladores/PortalControlador.java
@@ -3,54 +3,60 @@
 package com.equipoh.hservicios.controladores;
 
 
+import com.equipoh.hservicios.entidades.Proveedor;
 import com.equipoh.hservicios.entidades.Usuario;
 import com.equipoh.hservicios.servicios.UsuarioServicio;
 import java.util.List;
+import com.equipoh.hservicios.repositorios.ProveedorRepositorio;
+import com.equipoh.hservicios.servicios.UsuarioServicio;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import java.util.List;
 
 @Controller
 @RequestMapping("/")
 public class PortalControlador {
 
    
- @Autowired
+    @Autowired
     private UsuarioServicio usuarioServicio;
    
+    @Autowired
+    private ProveedorRepositorio proveedorRepositorio;
+
     @GetMapping("/")
     public String index(ModelMap modelo) {
         List<Usuario> usuarios = usuarioServicio.listarUsuario();
         modelo.addAttribute("usuarios", usuarios);
-
-     return "index.html";
+        return "index.html";
     }
 
-    @GetMapping("/registrar")
-    public String registrar() {
-        return "registrar.html";
-    }
     @GetMapping("/login")
     public String login() {
         return "login.html";
     }
+
     @GetMapping("/panel")
     public String panel() {
         return "panel.html";
     }
+
     @GetMapping("/list")
     public String listar(ModelMap modelo) {
         List<Usuario> usuarios = usuarioServicio.listarUsuario();
         modelo.addAttribute("usuarios", usuarios);
-
         return "listar_usuario.html";
     }
+
     @GetMapping("/active")
     public String active() {
         return "active.html";
     }
+
     @GetMapping("/modificar")
     public String modificar() {
         return "modificar_usuario.html";
@@ -61,7 +67,15 @@ public class PortalControlador {
         modelo.addAttribute("usuarios", usuarios);
         return "buscador.html";
     }
-     @GetMapping("/contrato")
+
+    @PostMapping("/encontrados")
+    public String buscador(String buscar, ModelMap model) {
+        List<Proveedor> listado = proveedorRepositorio.buscaProveedor(buscar);
+        model.addAttribute("proveedores", listado);
+        return "listar_proveedor";
+    }
+
+    @GetMapping("/contrato")
     public String contrato() {
         return "contratarService.html";
     }
---
+++ src/main/java/com/equipoh/hservicios/repositorios/ProveedorRepositorio.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.equipoh.hservicios.repositorios;
+
+import com.equipoh.hservicios.entidades.Proveedor;
+import java.util.List;
+
+/**
+ *
+ * @author manie
+ */
+public class ProveedorRepositorio {
+
+    public List<Proveedor> buscaProveedor(String buscar) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
