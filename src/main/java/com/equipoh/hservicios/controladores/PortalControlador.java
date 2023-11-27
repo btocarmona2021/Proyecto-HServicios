7a582c4bbbe3d5521ff78b748a9c710574746264
Alberto C <btocarmona@gmail.com>
2023-11-27 09:52:52 -0300

    Se agregó sistema valoración

--- src/main/java/com/equipoh/hservicios/controladores/PortalControlador.java
+++ src/main/java/com/equipoh/hservicios/controladores/PortalControlador.java
@@ -5,12 +5,9 @@ package com.equipoh.hservicios.controladores;
 
 import com.equipoh.hservicios.entidades.Contrato;
 import com.equipoh.hservicios.entidades.Proveedor;
-import com.equipoh.hservicios.entidades.Servicio;
 import com.equipoh.hservicios.entidades.Usuario;
 import com.equipoh.hservicios.repositorios.ContratoRepositorio;
 import com.equipoh.hservicios.repositorios.ProveedorRepositorio;
-import com.equipoh.hservicios.servicios.ProveedorServicio;
-import com.equipoh.hservicios.servicios.ServicioServicio;
 import com.equipoh.hservicios.servicios.UsuarioServicio;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
@@ -22,7 +19,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.servlet.http.HttpSession;
 import java.util.List;
-import org.springframework.web.bind.annotation.PathVariable;
 
 
 @Controller
@@ -31,17 +27,11 @@ public class PortalControlador {
 
 
     @Autowired
-    private ProveedorServicio proveedorServicio;
-     @Autowired
-    private ServicioServicio servicioServicio;
-    
-    @Autowired
     private UsuarioServicio usuarioServicio;
     @Autowired
     private ProveedorRepositorio proveedorRepositorio;
     @Autowired
     private ContratoRepositorio contratoRepositorio;
-    
 
     @GetMapping("/")
     public String index(ModelMap modelo) {
@@ -62,6 +52,11 @@ public class PortalControlador {
     }
 
 
+    @GetMapping("/active")
+    public String active() {
+        return "active.html";
+    }
+
 
     @GetMapping("/buscador")
     public String buscador() {
@@ -73,32 +68,30 @@ public class PortalControlador {
         List<Proveedor> listado = proveedorRepositorio.buscaProveedor(buscar);
         model.addAttribute("proveedores", listado);
         return "buscador";
-       /* return "listar_proveedor";*/
+        /* return "listar_proveedor";*/
     }
 
     @GetMapping("/contrato")
     public String contrato() {
         return "contratarService.html";
     }
-     @GetMapping("/nosotros")
+
+    @GetMapping("/nosotros")
     public String nosotros() {
         return "equipoh.html";
     }
-    
+
     @GetMapping("/inicio")
-    public String inicio(HttpSession session, ModelMap modelo) {
-        
-        
+    public String inicio(HttpSession session) {
+
         Usuario logueado = (Usuario) session.getAttribute("usuariosession");
-        
+
         if (logueado.getRol().toString().equals("ADMIN")) {
             return "redirect:/admin/dashboard";
         }
-         List<Servicio> servicios = servicioServicio.listarServicios();
-        modelo.addAttribute("servicios", servicios);
-           return "panel.html";
+        return "index.html";
     }
-    
+
     @PreAuthorize("hasAnyRole('ROLE_USUARIO', 'ROLE_ADMIN','ROLE_PROVEEDOR')")
     @GetMapping("/perfil")
     public String perfil(HttpSession session, ModelMap modelo) {
@@ -125,17 +118,5 @@ public class PortalControlador {
         modelo.addAttribute("contratos", contratos);
         return "perfilu";
     }
-    @PreAuthorize("hasAnyRole('ROLE_USUARIO', 'ROLE_ADMIN','ROLE_PROVEEDOR')")
-    @GetMapping("/perfil/complete/{id}")
-    public String perfil_proveedor(HttpSession session,@PathVariable(required = false) String id, ModelMap modelo) {
-       Usuario usuario = (Usuario) session.getAttribute("usuariosession");
-        List<Contrato> contratos = contratoRepositorio.buscaContratoSinAceptar(usuario.getId());
-        modelo.put("usuario", usuario);
-        modelo.addAttribute("contratos", contratos);
-        modelo.put("proveedores", proveedorServicio.getOne(id));
-       
-        return "perfil_proveedor.html";
-    }
-    
+
 }
- 
