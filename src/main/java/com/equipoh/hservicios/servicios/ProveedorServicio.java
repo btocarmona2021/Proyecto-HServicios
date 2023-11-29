fd847424d7d8faae2bda775c34a24674d70d9a13
Alberto C <btocarmona@gmail.com>
2023-11-29 02:12:22 -0300

    Union Rama Alberto Jorge ultimo

--- src/main/java/com/equipoh/hservicios/servicios/ProveedorServicio.java
+++ src/main/java/com/equipoh/hservicios/servicios/ProveedorServicio.java
@@ -129,11 +129,6 @@ public class ProveedorServicio {
         proveedor.setAlta(true);
         proveedor.setFechaAlta(usuario.getFechaAlta());
         proveedor.setRol(Rol.PROVEEDOR);
-        
-        SolicitudRol solicitudRol = solicitudRolServicio.getOne(idSolicitud);
-        proveedor.setExperiencia(solicitudRol.getExperiencia());
-        proveedor.setPrecioXHora(solicitudRol.getPrecioXHora());
-        proveedor.setServicio(solicitudRol.getServicio());
 
         SolicitudRol solicitudRol = solicitudRolServicio.getOne(idSolicitud);
         proveedor.setExperiencia(solicitudRol.getExperiencia());
