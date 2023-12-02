4855a40f10aacabe691cc8bfb277956c9280cffc
Alejandro Salerno <lales38@hotmail.com>
2023-12-02 20:34:45 -0300

    Sabado20231202

--- src/main/java/com/equipoh/hservicios/repositorios/ProveedorRepositorio.java
+++ src/main/java/com/equipoh/hservicios/repositorios/ProveedorRepositorio.java
@@ -1,4 +1,7 @@
-
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
 package com.equipoh.hservicios.repositorios;
 
 import com.equipoh.hservicios.entidades.Proveedor;
